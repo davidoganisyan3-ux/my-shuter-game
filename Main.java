187cf1af82761ca9178a72943e01b978314b9296
David <davidoganisyan3@gmail.com>
2026-09-25 01:53:55 +0300

    k

--- Main.java
+++ Main.java
@@ -5,6 +5,7 @@ import java.awt.*;
 
 public  class  Main {
     //Запуск
+    //pppp
     public static void main(String[] args) {
         JFrame frame = new JFrame("Shuter");
         frame.setSize(700, 700);
